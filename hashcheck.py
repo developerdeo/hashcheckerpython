c24abbaaba5a98a72819eb5a1c2a7d1cc7aa4267
Okello Deogratius <okdeowork@gmail.com>
2023-06-19 22:00:36 +0300

    text changed

--- hashcheck.py
+++ hashcheck.py
@@ -4,7 +4,7 @@ while x=='0':
     ONE=input("Enter the HASH 1 to be compared: ")
     
     while y==0:
-        any=input("\nUse power GET-FILEHASH then the name of the file, compare with\nthe one on the website\n\n        press Enter to continue")
+        any=input("\nUse powershell in windows command\nIn the directory of the file\n GET-FILEHASH then the name of the file, compare with\nthe one on the website\n Or in linux type google it\n\n        press Enter to continue")
         y +=1
     TWO=input("\nEnter the HASH 2 to be compared: ")
     if ONE==TWO:
